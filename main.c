fa1268ca529706cd08628cfed8ca6b3bb951d9b4
Martin Heini <maheini35@me.com>
2024-04-29 23:24:38 +0200

    example usage

--- main.c
+++ main.c
@@ -5,17 +5,47 @@
  * Author : Martin
  */ 
 
-#define F_CPU 2000000	// Set clock at 2MHZ
-
 #include <avr/io.h>
+#define F_CPU 10000000
+#include <util/delay.h>
 
 #include "blinkcode.h"
-
+#include "pwm/pwm.h"
+#include "prescaler/prescaler.h"
+#include "adc/adc.h"
 
 #define ADC_PIN PIN2_bm
 #define LED_PIN PIN1_bm
 
-int main(void){
-	set_prescaler(CLKCTRL_PDIV_2X_gc);
-	blink_now(LED_PIN, 1000);
+float get_dd(){
+	return 5.2;
 }
+
+int main(void){
+	set_prescaler(CLKCTRL_PDIV_10X_gc);
+	pwm_start(100, 0);
+	
+	adc_prepare(PIN2_bm);
+	
+	float stepcount = 100 / 4.3;
+	
+	while(1){
+		pwm_adjust(adc_get_v_reading() * stepcount);
+	}
+	
+	int counter = 50;
+	int upwards = 1;
+	while(1){
+		pwm_adjust(counter);
+		
+		if (counter >= 100){
+			upwards = 0;
+		} else if (counter <= 50){
+			upwards = 1;
+		}
+		counter = upwards ? counter + 1 : counter - 1;
+		
+		_delay_ms(200);
+	}
+}
+
