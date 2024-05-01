7f04092a5fba81e3d04d3a79b50ceebc3c32584d
Martin Heini <maheini35@me.com>
2024-05-02 00:32:27 +0200

    setup logic implemented

--- buttonpress/buttonpress.c
+++ buttonpress/buttonpress.c
@@ -14,3 +14,31 @@ void listen_pin(uint8_t pin, void (*callback)()){
 	uint8_t pinctrl_cfg = PORT_PULLUPEN_bm | PORT_ISC_FALLING_gc;
 	PORTA.DIR &= ~pin;
 	
+	switch(pin){
+		case PIN0_bm:
+			PORTA_PIN0CTRL = pinctrl_cfg;
+			registered_callback[0] = callback;
+			break;
+		case PIN1_bm:
+			PORTA_PIN1CTRL = pinctrl_cfg;
+			registered_callback[1] = callback;
+			break;
+		case PIN2_bm:
+			PORTA_PIN2CTRL = pinctrl_cfg;
+			registered_callback[2] = callback;
+			break;
+		case PIN3_bm:
+			PORTA_PIN3CTRL = pinctrl_cfg;
+			registered_callback[3] = callback;
+			break;
+		case PIN6_bm:
+			PORTA_PIN6CTRL = pinctrl_cfg;
+			registered_callback[6] = callback;
+			break;
+		case PIN7_bm:
+			PORTA_PIN7CTRL = pinctrl_cfg;
+			registered_callback[7] = callback;
+			break;
+	}
+	sei();
+}
